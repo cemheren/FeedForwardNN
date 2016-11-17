aaf519f9023cae11b60795266d0f0bf78b4ee445
Akif Heren <akhe@microsoft.com>
2016-11-16 17:58:39 -0800

    change root path

---
+++ nnExample/FeedForwardNetwork.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nnExample
+{
+    public class FeedForwardNetwork
+    {
+        private Neuron[] inputNeurons;
+        private Neuron[] hiddenNeurons;
+        private Neuron[] outputNeurons;
+
+        private Random rand = new Random(71);
+
+        public double LearningRate
+        {
+            get
+            {
+                return 0.3;
+                return (0.2 + (rand.NextDouble()));
+            }
+        }
+
+        public FeedForwardNetwork(int input, int hidden, int output)
+        {
+            input++;
+            hidden++;
+
+            inputNeurons = new Neuron[input];
+            for (int i = 0; i < input; i++)
+            {
+                inputNeurons[i] = new Neuron(0);
+            }
+
+            hiddenNeurons = new Neuron[hidden];
+            for (int i = 0; i < hidden; i++)
+            {
+                hiddenNeurons[i] = new Neuron(input);
+            }
+
+            outputNeurons = new Neuron[output];
+            for (int i = 0; i < output; i++)
+            {
+                outputNeurons[0] = new Neuron(hidden);
+            }
+        }
+
+        public override string ToString()
+        {
+            var s = new StringBuilder();
+
+            s.AppendLine();
+
+            foreach (var neuron in hiddenNeurons)
+            {
+                foreach (var weight in neuron.Weights)
+                {
+                    s.Append(weight + " ");
+                }
+
+                s.Append("|| ");
+            }
+
+            s.AppendLine("------------------------------------------------------------------");
+
+            foreach (var neuron in outputNeurons)
+            {
+                foreach (var weight in neuron.Weights)
+                {
+                    s.Append(weight + " ");
+                }
+
+                s.Append("|| ");
+            }
+
+            return s.ToString();
+        }
+
+        public double[] GetCurrentResult()
+        {
+            return outputNeurons.Select(n => n.currentValue).ToArray();
+        }
+
+        public void ForwardPass(double[] input)
+        {
+            // keep inputs in the network because we use them later in back propagation
+            for (int i = 0; i < this.inputNeurons.Length - 1; i++)
+            {
+                this.inputNeurons[i].currentValue = input[i];
+            }
+            this.inputNeurons.Last().currentValue = 1; // bias node
+
+            for (int i = 0; i < hiddenNeurons.Length - 1; i++)
+            {
+                hiddenNeurons[i].ActivateNeuron(this.inputNeurons.Select(n => n.currentValue).ToArray());
+            }
+            hiddenNeurons.Last().currentValue = 1; // bias node
+
+            foreach (var neuron in outputNeurons)
+            {
+                neuron.ActivateNeuron(hiddenNeurons.Select(n => n.currentValue).ToArray());
+            }
+        }
+
+        public void BackPropagateForTarget(double[] target)
+        {
+            // Get the delta value for the output layer
+            for (int i = 0; i < this.outputNeurons.Length; i++)
+            {
+                this.outputNeurons[i].SetMomentumErrorProduct(target[i] - this.outputNeurons[i].currentValue);
+            }
+
+            for (int i = 0; i < this.hiddenNeurons.Length; i++)
+            {
+                double error = 0.0;
+                for (int j = 0; j < this.outputNeurons.Length; j++)
+                {
+                    error += this.outputNeurons[j].Weights[i] * this.outputNeurons[j].CurrentMomentumErrorProduct;
+                }
+                this.hiddenNeurons[i].SetMomentumErrorProduct(error);
+            }
+            // we propagated the errors back
+
+            // Now update the weights between hidden & output layer
+            UpdateWeights(this.outputNeurons, this.hiddenNeurons);
+
+            // Now update the weights between input & hidden layer
+            UpdateWeights(this.hiddenNeurons, this.inputNeurons);
+        }
+
+        private void UpdateWeights(Neuron[] outputLayer, Neuron[] inputLayer)
+        {
+            for (int i = 0; i < outputLayer.Length; i++)
+            {
+                for (int j = 0; j < inputLayer.Length; j++)
+                {
+                    outputLayer[i].Weights[j] += this.LearningRate * outputLayer[i].CurrentMomentumErrorProduct * inputLayer[j].currentValue;
+                }
+            }
+        }
+    }
+}
---
+++ nnExample/SineWaveApproximator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nnExample
+{
+    public class SineWaveApproximator
+    {
+        public static void M()
+        {
+            var nn = new FeedForwardNetwork(1, 20, 1);
+            var rand = new Random(13);
+
+            for (int i = 0; i < 50000; i++)
+            {
+                var t = rand.NextDouble();
+                nn.ForwardPass(new double[] { t });
+                nn.BackPropagateForTarget(new double[] { Math.Sin(t) });
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                var t = rand.NextDouble();
+                nn.ForwardPass(new double[] { t });
+
+                Console.WriteLine(Math.Sin(t));
+                Console.WriteLine(nn.GetCurrentResult()[0]);
+                Console.WriteLine("==============================");
+            }
+
+            Console.WriteLine(nn.ToString());
+        }
+    }
+}
