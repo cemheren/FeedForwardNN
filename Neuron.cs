3947a7c93d300a3be5dba96b40e5c89d7bf0227c
Akif Heren <akhe@microsoft.com>
2016-11-16 16:57:12 -0800

    minor refactoring

---
+++ Neuron.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nnExample
+{
+    public class Neuron
+    {
+        private Random rand = new Random(13);
+
+        public double[] Weights;
+        public double currentValue;
+
+        public double CurrentMomentumErrorProduct;
+
+        public Neuron()
+        {
+            Weights = new double[0];
+        }
+        public Neuron(int inputs)
+        {
+            Weights = new double[inputs];
+            for (int i = 0; i < Weights.Length; i++)
+            {
+                Weights[i] = (rand.NextDouble() * 4) - 2;
+            }
+        }
+
+        public void ActivateNeuron(double[] input)
+        {
+            var sum = 0.0;
+            for (int i = 0; i < this.Weights.Length; i++)
+            {
+                sum += input[i] * this.Weights[i];
+            }
+            this.currentValue = Sigmoid(sum);
+        }
+
+        public void SetDeltaFromError(double error)
+        {
+            CurrentMomentumErrorProduct = currentValue * (1.0 - currentValue) * (error);
+        }
+
+        public static double Sigmoid(double input)
+        {
+            return 1.0 / (1.0 + Math.Exp(-1 * input));
+        }
+    }
+}
