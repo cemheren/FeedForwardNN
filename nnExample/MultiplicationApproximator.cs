dee5dc67709d3861af018583c9bb6d78ae5842d0
Akif Heren <akhe@microsoft.com>
2016-11-17 16:58:06 -0800

    add mnist

---
+++ nnExample/MultiplicationApproximator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nnExample
+{
+    public class MultiplicationApproximator
+    {
+        public static void M()
+        {
+            var nn = new FeedForwardNetwork(2, 6, 1);
+            var rand = new Random(13);
+
+            for (int i = 0; i < 500000; i++)
+            {
+                var t1 = rand.NextDouble();
+                var t2 = rand.NextDouble();
+
+                nn.ForwardPass(new double[] { t1, t2 });
+                nn.BackPropagateForTarget(new double[] { (t1 * t2) });
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                var t1 = rand.NextDouble();
+                var t2 = rand.NextDouble();
+
+                nn.ForwardPass(new double[] { t1, t2 });
+
+                Console.WriteLine((t1 * t2));
+                Console.WriteLine(nn.GetCurrentResult()[0]);
+                Console.WriteLine("==============================");
+            }
+
+            Console.WriteLine(nn.ToString());
+        }
+    }
+}
